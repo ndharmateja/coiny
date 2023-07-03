d59428b58f990946b746bcd2f1c6d4f03dcbc5f7
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-07-04 04:09:32 +0530

    Rename API_URL to GET_KEY_URL

--- bin/coindex-key.js
+++ bin/coindex-key.js
@@ -1,12 +1,12 @@
 import { Command } from "commander";
 import { key } from "../handlers/key-handler.js";
-import { API_URL } from "../utils/constants.js";
+import { GET_KEY_URL } from "../utils/constants.js";
 
 const program = new Command();
 
 program
   .command("set")
-  .description(`Set API Key -> Get at ${API_URL}`)
+  .description(`Set API Key -> Get at ${GET_KEY_URL}`)
   .action(key.set);
 
 program.command("show").description("Show API Key").action(key.show);
