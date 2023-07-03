1e39f567baefb791fd1cf5cda1162f8b91f5b9cf
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-07-04 04:15:08 +0530

    Update api base url

--- utils/constants.js
+++ utils/constants.js
@@ -1,2 +1,3 @@
 export const GET_KEY_URL = "https://www.cryptocompare.com/cryptopian/api-keys";
-export const API_BASE_URL = "https://www.cryptocompare.com/cryptopian/api-keys";
+export const API_BASE_URL =
+  "https://min-api.cryptocompare.com/data/pricemultifull";
