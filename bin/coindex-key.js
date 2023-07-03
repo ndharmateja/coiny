fc14add1526ffa30ea350ebb74f6521e3cd8dcbf
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-07-04 02:07:36 +0530

    Setup handlers for subcommands of key

--- bin/coindex-key.js
+++ bin/coindex-key.js
@@ -1,20 +1,15 @@
 import { Command } from "commander";
+import { key } from "../commands/key.js";
 
 const program = new Command();
 
 program
   .command("set")
   .description("Set API Key -> Get at https://www.coinapi.io")
-  .action(() => console.log("hello from set"));
+  .action(key.set);
 
-program
-  .command("show")
-  .description("Show API Key")
-  .action(() => console.log("hello from show"));
+program.command("show").description("Show API Key").action(key.show);
 
-program
-  .command("delete")
-  .description("Delete API Key")
-  .action(() => console.log("hello from delete"));
+program.command("delete").description("Delete API Key").action(key.delete);
 
 program.parse(process.argv);
---
+++ commands/key.js
@@ -0,0 +1,5 @@
+export const key = {
+  set: () => console.log("hello from set"),
+  show: () => console.log("hello from show"),
+  delete: () => console.log("hello from delete"),
+};
