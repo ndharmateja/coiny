43eeeeab552213015f56a9e302011ed1ac3396e6
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-07-04 04:06:59 +0530

    Rename API_URL to GET_KEY_URL

--- bin/coindex.js
+++ bin/coindex.js
@@ -3,7 +3,7 @@
 // Imports
 import { Command } from "commander";
 import { createRequire } from "module";
-import { API_URL } from "../utils/constants.js";
+import { GET_KEY_URL } from "../utils/constants.js";
 const require = createRequire(import.meta.url);
 const pkg = require("../package.json");
 
@@ -11,6 +11,6 @@ const program = new Command();
 
 program
   .version(pkg.version)
-  .command("key", `Manage API key -> Get at ${API_URL}`)
+  .command("key", `Manage API key -> Get at ${GET_KEY_URL}`)
   .command("check", "Check cryptocurrency coins prices")
   .parse(process.argv);
