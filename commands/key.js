35ff63aafb11b4f23077feaae89477522a8a8efc
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-07-04 02:51:07 +0530

    Add functionality to set, show and delete key

--- commands/key.js
+++ commands/key.js
@@ -1,5 +1,59 @@
+import { KeyManager } from "../lib/KeyManager.js";
+import inquirer from "inquirer";
+import colors from "colors";
+const keyManager = new KeyManager();
+
+async function takeKeyInput() {
+  let input = await inquirer.prompt([
+    {
+      type: "input",
+      name: "key",
+      message: "(https://www.coinapi.io) ".yellow + "Enter API Key:".green,
+    },
+  ]);
+
+  // While key is empty, show error message and take input again
+  do {
+    console.log("Key cannot be empty.".red);
+    input = await inquirer.prompt([
+      {
+        type: "input",
+        name: "key",
+        message: "(https://www.coinapi.io) ".yellow + "Enter API Key:".green,
+      },
+    ]);
+  } while (!input.key.trim());
+
+  // Return the input
+  return input.key.trim();
+}
+
 export const key = {
-  set: () => console.log("hello from set"),
-  show: () => console.log("hello from show"),
-  remove: () => console.log("hello from remove"),
+  set: async () => {
+    const key = await takeKeyInput();
+
+    // Set key
+    keyManager.setKey(key);
+    console.log("Successfully set the key.".green);
+  },
+  show: () => {
+    try {
+      // Try to fetch the key
+      const key = keyManager.getKey();
+      console.log("API Key: ".blue + key.green);
+    } catch (error) {
+      // If error show error message
+      console.log(error.message.red);
+    }
+  },
+  remove: () => {
+    try {
+      // Try to remove the key
+      keyManager.deleteKey();
+      console.log("Successfully remove the key.".green);
+    } catch (error) {
+      // If error show error message
+      console.log(error.message.red);
+    }
+  },
 };
