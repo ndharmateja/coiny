20de3620fcf5ae5165f5a25dd43eda5de5d85d81
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-07-05 04:48:58 +0530

    Rename coinName to coinCode

--- handlers/check-handler.js
+++ handlers/check-handler.js
@@ -7,7 +7,7 @@ const getOutputString = (coinDataList) => {
   let output = "Prices List:\n";
   for (const coinData of coinDataList) {
     output += `Coin: `;
-    output += `${coinData.coinName}`.yellow;
+    output += `${coinData.coinCode}`.yellow;
     output += ` | Price: `;
     output += `${coinData.coinPrice}\n`.green;
   }
