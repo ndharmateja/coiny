92f2484c5bca014d8db5a020b7c4b200b0bea06a
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-07-05 03:54:17 +0530

    Refactor code to use CoinData class

--- handlers/check-handler.js
+++ handlers/check-handler.js
@@ -2,15 +2,13 @@ import { KeyManager } from "../lib/KeyManager.js";
 import { CryptoApi } from "../lib/CryptoApi.js";
 import colors from "colors";
 
-const getOutputString = (displayData, curr) => {
+const getOutputString = (coinDataList) => {
   let output = "Prices List:\n";
-  for (const coin in displayData) {
-    const data = displayData[coin][curr.toUpperCase()];
-    const { PRICE: price } = data;
+  for (const coinData of coinDataList) {
     output += `Coin: `;
-    output += `${coin}`.yellow;
+    output += `${coinData.coinName}`.yellow;
     output += ` | Price: `;
-    output += `${price}\n`.green;
+    output += `${coinData.coinPrice}\n`.green;
   }
   return output;
 };
@@ -33,12 +31,12 @@ export const check = {
 
     try {
       const api = new CryptoApi(key);
-      const { DISPLAY: displayData } = await api.getPriceData(
-        cmd.coin,
-        cmd.curr
-      );
 
-      const output = getOutputString(displayData, cmd.curr);
+      // CoinData[]
+      const coinDataList = await api.getPriceData(cmd.coin, cmd.curr);
+      const output = getOutputString(coinDataList);
+
+      // Print output to console without new line
       process.stdout.write(output);
     } catch (error) {
       console.log(error.message.red);
