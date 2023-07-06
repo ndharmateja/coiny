e16f89fc9f522b0ee5629dcd5fc5d180327b8d74
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-07-06 06:23:42 +0530

    Refactor check handler
    - Separate functions for parsing options and getting key
    - same try catch block for all errors

--- handlers/check-handler.js
+++ handlers/check-handler.js
@@ -5,59 +5,54 @@ import { validateCurrency } from "../utils/validation.js";
 import { printCoinDataList } from "../utils/output-generator.js";
 import { DEFAULT_COINS } from "../utils/constants.js";
 
+const parseOptions = (cmd) => {
+  // If not valid currency, validateCurrency throws error
+  const curr = validateCurrency(cmd.curr);
+
+  let isTop;
+  let topLimit;
+  let coin;
+  if (cmd.coin && cmd.top) {
+    // Both coin and top options are not allowed
+    throw new Error("Cannot specify both --top and --coin options");
+  } else if (cmd.coin) {
+    // If only coin is specified
+    isTop = false;
+    coin = cmd.coin;
+  } else if (cmd.top) {
+    // If only top is specified
+    isTop = true;
+    topLimit = parseInt(cmd.top);
+    if (!topLimit) {
+      throw new Error(`--top value not a valid integer: ${cmd.top}`);
+    }
+  } else {
+    // If both are not specified, we will get top 10 coins
+    isTop = true;
+    topLimit = 10;
+  }
+
+  return { curr, isTop, topLimit, coin };
+};
+
+const getKey = () => {
+  const keyManager = new KeyManager();
+  let key;
+  try {
+    key = keyManager.getKey();
+  } catch (error) {
+    throw new Error(
+      "API Key not set. Set API Key using 'coiny key set' and then check prices."
+    );
+  }
+};
+
 export const check = {
   price: async (cmd) => {
-    const keyManager = new KeyManager();
-
-    let key;
     try {
-      key = keyManager.getKey();
-    } catch (error) {
-      console.log(
-        "API Key not set. Set API Key using ".red +
-          "coiny key set".yellow +
-          " and then check prices.".red
-      );
-      return;
-    }
-
-    // If not valid currency
-    let curr;
-    try {
-      curr = validateCurrency(cmd.curr);
-    } catch (error) {
-      console.log(error.message.red);
-      return;
-    }
-
-    // Parse options
-    let isTop;
-    let topLimit;
-    let coin;
-    if (cmd.coin && cmd.top) {
-      // Both coin and top options are not allowed
-      console.log("Cannot specify both --top and --coin options".red);
-      return;
-    } else if (cmd.coin) {
-      // If only coin is specified
-      isTop = false;
-      coin = cmd.coin;
-    } else if (cmd.top) {
-      // If only top is specified
-      isTop = true;
-      topLimit = parseInt(cmd.top);
-      if (!topLimit) {
-        console.log(`--top value not a valid integer: ${cmd.top}`.red);
-        return;
-      }
-    } else {
-      // If both are not specified, we will get top 10 coins
-      isTop = true;
-      topLimit = 10;
-    }
-
-    try {
-      const api = new CryptoApi(key);
+      const apiKey = getKey();
+      const { isTop, topLimit, curr, coin } = parseOptions(cmd);
+      const api = new CryptoApi(apiKey);
 
       // CoinData[]
       const coinDataList = isTop
